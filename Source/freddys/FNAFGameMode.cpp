cfb54a42e7ec3e2f2fdc14c670f4dea40a87f829
ruby <rubyperrin134@gmail.com>
2026-03-01 21:15:43 +0000

    wip gamemode

--- Source/freddys/FNAFGameMode.cpp
+++ Source/freddys/FNAFGameMode.cpp
@@ -1,12 +1,6 @@
 #include "FNAFGameMode.h"
 
-AFNAFGameMode::AFNAFGameMode(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-    this->LevelDBClass = NULL;
-    this->LevelDBInstance = NULL;
+void AFNAFGameMode::SpawnLevelDB()
+{
+	
 }
-
-void AFNAFGameMode::SpawnLevelDB() {
-}
-
-
-
--- Source/freddys/FNAFGameMode.h
+++ Source/freddys/FNAFGameMode.h
@@ -1,28 +1,27 @@
 #pragma once
+
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
-#include "Templates/SubclassOf.h"
 #include "FNAFGameMode.generated.h"
 
 class ULevelDB;
 
 UCLASS(NonTransient)
-class FREDDYS_API AFNAFGameMode : public AGameMode {
+class FREDDYS_API AFNAFGameMode : public AGameMode
+{
     GENERATED_BODY()
+    
 public:
     UPROPERTY(BlueprintReadWrite, EditAnywhere)
     TSubclassOf<ULevelDB> LevelDBClass;
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    ULevelDB* LevelDBInstance;
+	ULevelDB* LevelDBInstance;
     
-    AFNAFGameMode(const FObjectInitializer& ObjectInitializer);
-
     UFUNCTION(BlueprintCallable)
     void SpawnLevelDB();
     
     UFUNCTION(BlueprintImplementableEvent)
     void OnSetLevelDB();
-    
 };
 
