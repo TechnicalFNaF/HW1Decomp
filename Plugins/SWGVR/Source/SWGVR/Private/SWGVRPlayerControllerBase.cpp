6871e2026aa28f749d24629a4c7d7df3c5ee8ded
Adam <reallyfakeadam@gmail.com>
2026-03-02 15:10:31 +0000

    ASWGVRPlayerControllerBase Done *

--- Plugins/SWGVR/Source/SWGVR/Private/SWGVRPlayerControllerBase.cpp
+++ Plugins/SWGVR/Source/SWGVR/Private/SWGVRPlayerControllerBase.cpp
@@ -1,14 +1,48 @@
 #include "SWGVRPlayerControllerBase.h"
+#include "SWGVRCharacter.h"
 
-ASWGVRPlayerControllerBase::ASWGVRPlayerControllerBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-    this->ClickEventKeys.AddDefaulted(1);
+bool ASWGVRPlayerControllerBase::bIsUsingGamepad = false;
+
+bool ASWGVRPlayerControllerBase::InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad)
+{
+	// I think this is accurate
+	if (!bGamepad)
+	{
+		if (Delta <= 1.0f)
+		{
+			return Super::InputAxis(Key, Delta, DeltaTime, NumSamples, bGamepad);
+		}
+		UpdateUsingGamepadState(bGamepad);
+	}
+
+	if (Delta > 0.5)
+		UpdateUsingGamepadState(bGamepad);
+
+	return Super::InputAxis(Key, Delta, DeltaTime, NumSamples, bGamepad);
 }
 
-void ASWGVRPlayerControllerBase::OnUsingGamepadChanged_Implementation() {
+bool ASWGVRPlayerControllerBase::InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad)
+{
+	UpdateUsingGamepadState(bGamepad);
+
+	return Super::InputKey(Key, EventType, AmountDepressed, bGamepad);
 }
 
-bool ASWGVRPlayerControllerBase::IsUsingGamepad() const {
-    return false;
+void ASWGVRPlayerControllerBase::OnUsingGamepadChanged_Implementation()
+{
+	// Not implemented in game
 }
 
+void ASWGVRPlayerControllerBase::UpdateUsingGamepadState(bool bUsingGamepad)
+{
+	if (bUsingGamepad != GetStaticIsUsingGamepad())
+	{
+		bIsUsingGamepad = bUsingGamepad;
+		OnUsingGamepadChanged();
 
+		if (ASWGVRCharacter* SWGCharacter = Cast<ASWGVRCharacter>(GetPawn()))
+		{
+			SWGCharacter->OnControllerDeviceChanged.Broadcast();
+		}
+	}
+}
--- Plugins/SWGVR/Source/SWGVR/Public/SWGVRPlayerControllerBase.h
+++ Plugins/SWGVR/Source/SWGVR/Public/SWGVRPlayerControllerBase.h
@@ -4,18 +4,33 @@
 #include "SWGVRPlayerControllerBase.generated.h"
 
 UCLASS()
-class SWGVR_API ASWGVRPlayerControllerBase : public APlayerController {
+class SWGVR_API ASWGVRPlayerControllerBase : public APlayerController 
+{
     GENERATED_BODY()
 public:
-    ASWGVRPlayerControllerBase(const FObjectInitializer& ObjectInitializer);
+
+	static FORCEINLINE bool GetStaticIsUsingGamepad()
+	{
+		return bIsUsingGamepad;
+	};
+
+	UFUNCTION(BlueprintPure)
+	bool IsUsingGamepad()
+	{
+		return GetStaticIsUsingGamepad();
+	};
+	
+	virtual bool InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad) override;
+	virtual bool InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad) override;
 
 protected:
-    UFUNCTION(BlueprintNativeEvent)
-    void OnUsingGamepadChanged();
-    
-public:
-    UFUNCTION(BlueprintPure)
-    bool IsUsingGamepad() const;
-    
-};
 
+	UFUNCTION(BlueprintNativeEvent)
+	void OnUsingGamepadChanged();
+
+private:
+	
+	void UpdateUsingGamepadState(bool);
+
+	static bool bIsUsingGamepad;
+};
