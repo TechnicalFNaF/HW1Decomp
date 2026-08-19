d7b3c30fb2369d0a700838e4ce24de08f536c1a9
Adam <reallyfakeadam@gmail.com>
2026-08-19 04:01:19 +0100

    USWGPlatformUtilFunctions Matching

--- Plugins/SWGPlatformUtil/Source/SWGPlatformUtil/Private/SWGPlatformUtilFunctions.cpp
+++ Plugins/SWGPlatformUtil/Source/SWGPlatformUtil/Private/SWGPlatformUtilFunctions.cpp
@@ -2,46 +2,9 @@
 
 #include "SWGPlatformSettings.h"
 
-void USWGPlatformUtilFunctions::SwitchOnPlatform(ESWGPlatform& Platform) 
+// Matching
+const FString& USWGPlatformUtilFunctions::GetVersionString() 
 {
-	Platform = GetBuildPlatform();
-}
-
-void USWGPlatformUtilFunctions::IsInEditorSwitch(ESWGEditor& Editor) 
-{
-	Editor = IsInEditor() ? ESWGEditor::InEditor : ESWGEditor::NotInEditor;
-}
-
-bool USWGPlatformUtilFunctions::IsInEditor() 
-{
-	return FPlatformProperties::HasEditorOnlyData();
-}
-
-FString USWGPlatformUtilFunctions::GetVersionString() 
-{
-	static FString VersionString = TEXT("unversioned");
+	static FString VersionString = {TEXT("unversioned")};
 	return VersionString;
-}
-
-ESWGPlatform USWGPlatformUtilFunctions::GetBuildPlatform() 
-{
-#if PLATFORM_WINDOWS 
-	return ESWGPlatform::Steam;
-#endif
-
-#if PLATFORM_ANDROID
-	return ESWGPlatform::Oculus;
-#endif
-
-#if PLATFORM_PS4
-	return ESWGPlatform::PS4;
-#endif
-
-#if PLATFORM_SWITCH
-	return ESWGPlatform::Switch;
-#endif
-
-#if PLATFORM_XBOXONE
-	return ESWGPlatform::XBoxOne;
-#endif
 }
--- Plugins/SWGPlatformUtil/Source/SWGPlatformUtil/Public/SWGPlatformUtilFunctions.h
+++ Plugins/SWGPlatformUtil/Source/SWGPlatformUtil/Public/SWGPlatformUtilFunctions.h
@@ -1,5 +1,7 @@
 #pragma once
+
 #include "CoreMinimal.h"
+#include "SWGPlatformSettings.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "SWGPlatformUtilFunctions.generated.h"
 
@@ -11,18 +13,48 @@ class SWGPLATFORMUTIL_API USWGPlatformUtilFunctions : public UBlueprintFunctionL
 public:
 
 	UFUNCTION(BlueprintCallable, meta=(ExpandEnumAsExecs="Platform"))
-	static void SwitchOnPlatform(ESWGPlatform& Platform);
+	static void SwitchOnPlatform(ESWGPlatform& Platform)
+	{
+		Platform = GetBuildPlatform();
+	}
 	
 	UFUNCTION(BlueprintCallable, meta = (ExpandEnumAsExecs = "Editor"))
-	static void IsInEditorSwitch(ESWGEditor& Editor);
+	static void IsInEditorSwitch(ESWGEditor& Editor)
+	{
+		Editor = IsInEditor() ? ESWGEditor::InEditor : ESWGEditor::NotInEditor;
+	}
 	
 	UFUNCTION(BlueprintPure)
-	static bool IsInEditor();
+	static bool IsInEditor()
+	{
+		return FPlatformProperties::HasEditorOnlyData();
+	}
 	
 	UFUNCTION(BlueprintPure)
-	static FString GetVersionString();
+	static const FString& GetVersionString();
 	
 	UFUNCTION(BlueprintPure)
-	static ESWGPlatform GetBuildPlatform();
+	static ESWGPlatform GetBuildPlatform()
+	{
+#if PLATFORM_WINDOWS 
+		return ESWGPlatform::Steam;
+#endif
+
+#if PLATFORM_ANDROID
+		return ESWGPlatform::Oculus;
+#endif
+
+#if PLATFORM_PS4
+		return ESWGPlatform::PS4;
+#endif
+
+#if PLATFORM_SWITCH
+		return ESWGPlatform::Switch;
+#endif
+
+#if PLATFORM_XBOXONE
+		return ESWGPlatform::XBoxOne;
+#endif
+	}
 };
 
